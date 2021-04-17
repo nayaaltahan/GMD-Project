21ec642274187df1e8ddc9f8db38afd7d357cee1
Naya Al Tahan <naya.altahan00@gmail.com>
2021-04-17 19:21:21 +0200

    initial commit

---
+++ pink-panther/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    
+    public float smoothSpeed = 0.125f;
+
+    public Vector3 offset;
+
+    //https://www.youtube.com/watch?v=MFQhpwc6cKE&ab_channel=Brackeys
+    private void FixedUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
+    }
+}
---
+++ pink-panther/Assets/Scripts/CharacterScript.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using Vector2 = System.Numerics.Vector2;
+
+public class CharacterScript : MonoBehaviour
+{
+    [SerializeField] public Animator mainCharacterAnimator;
+    private SpriteRenderer mySpriteRenderer;
+    private float speed;
+
+    private void Start()
+    {
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        speed = (float) (transform.localScale.x * 0.7);
+    }
+
+    void Update()
+    {
+        Run();
+        float vertical = Input.GetAxis("Vertical");
+        if (vertical > 0)
+        {
+            mainCharacterAnimator.SetTrigger("Jump");
+        }
+        else
+        {
+            mainCharacterAnimator.ResetTrigger("Jump");
+        }
+    }
+
+    private void Run()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        if (horizontal != 0)
+        {
+            if (horizontal > 0)
+            {
+                mySpriteRenderer.flipX = false;
+                transform.position += Vector3.right * (speed * Time.deltaTime);
+            }
+
+            if (horizontal < 0)
+            {
+                mySpriteRenderer.flipX = true;
+                transform.position += Vector3.left * (speed * Time.deltaTime);
+            }
+
+            mainCharacterAnimator.SetBool("Move", true);
+        }
+        else
+        {
+            mainCharacterAnimator.SetBool("Move", false);
+        }
+    }
+}
